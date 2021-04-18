3ee838f14d7ebe8513a0c2b459410cd9e73c0459
fr1vol <fr1vol@outlook.com>
2021-04-19 06:07:48 +0800

    rb_tree need more test

--- practice/include/red_black_tree.h
+++ practice/include/red_black_tree.h
@@ -3,6 +3,8 @@
 
 #include <memory>
 #include <utility>
+#include <vector>
+#include <queue>
 namespace ez 
 {
 
@@ -34,13 +36,38 @@ public:
 
 
     red_black_tree():root(nullptr){}
-    ~red_black_tree(){delete root;}
+    ~red_black_tree(){
+        std::queue<rb_node*> q;
+        if(root != nullptr){ 
+            q.push(root);
+        }
+
+        while(!q.empty()){
+            int size = q.size();
+            for(int i = 0 ; i< size ; ++i){
+                
+                auto n = q.front();
+                q.pop();
+
+                if(n->left != nullptr){
+                    q.push(n->left);
+                }
+
+                if(n->right != nullptr){
+                    q.push(n->right);
+                }
+
+                delete n;
+            }
+        }
+    }
 
     int size(const rb_node* node) const{
         if(node == nullptr) return 0;
         int left_size = node->left == nullptr ? 0 : node->left->size;
         int right_size = node->right == nullptr ? 0 : node->right->size;
         return left_size + right_size + 1;
+        
     }
 
     int size() const {
@@ -57,19 +84,19 @@ public:
     }
     std::pair<V,bool> get(rb_node* node, const K& key){
 
-        if(node == nullptr) return std::pair<V,bool>{V{},false};
+        if(node == nullptr) return {V{},false};
         if(node->k == key){
-            return std::pair<V,bool>{node->v,true};
+            return {node->v,true};
         }else if(key < node->k ){
-            return std::pair<V,bool>(node->left,key);
+            return get(node->left,key);
         }else{
-            return std::pair<V,bool>(node->right,key);
+            return get(node->right,key);
         }
 
         return {V{},false};
     }
 
-    rb_node min(rb_node* node){
+    rb_node* min(rb_node* node){
         if(node->left == nullptr) return node;
         return min(node->left);
     }
@@ -84,41 +111,50 @@ public:
     rb_node* rotate_left(rb_node* node){
 
         rb_node* x = node->right;
+        if(x == nullptr) return node;
         rb_node* x_left = x->left;
         node->right = x_left;
+        
         x->left = node;
         x->color = node->color;
         node->color =rb_node:: red;
         x->size = node->size;
         node->size = size(node);
+        return x;
     }
 
     rb_node* rotate_right(rb_node* node){
 
+        
         rb_node* x = node->left;
+        if(x == nullptr) return node;
         rb_node* x_right = x->right;
         node->left = x_right;
+        
         x->right = node;
         x->color = node->color;
         node->color = rb_node::red;
         x->size = node->size;
         node->size = size(node);
+        return x;
     }
 
     void flip_color(rb_node* node){
-        node->color = rb_node::red;
-        node->left->color = rb_node::black;
-        node->right->color = rb_node::black;
+        node->color = (node->color == rb_node::red)?rb_node::black : rb_node::red;
+        node->left->color = (node->color == rb_node::red)?rb_node::black : rb_node::red;
+        node->right->color = (node->color == rb_node::red)?rb_node::black : rb_node::red;
     }
+
+  
     
     void put(K key,V val){
-        put(root,key,val);
+        root = put(root,key,val);
         root->color = rb_node::black;
     }
 
     rb_node* put(rb_node* node, K key,V val){
         if(node == nullptr){
-            node = new rb_node(key,val);//color: red ; size: 1 ;
+            node = new rb_node(key,val);
             return node;
         }        
 
@@ -127,7 +163,7 @@ public:
         }else if(key > node->k){
             node->right = put(node->right,key,val);
         }else{
-            node->val = val;
+            node->v= val;
         }
         
         if(is_red(node->right) && !is_red(node->left)) node = rotate_left(node);
@@ -140,7 +176,7 @@ public:
     }
 
     rb_node* balance(rb_node* node){
-        if(is_red(node->right) && !is_red(node->left)) node = rotate_left(node);
+        if(is_red(node->right)) node = rotate_left(node);
         if(is_red(node->left) && is_red(node->left->left)) node = rotate_right(node);
         if(is_red(node->right) && is_red(node->left)) flip_color(node);
         node->size = size(node);
@@ -158,6 +194,7 @@ public:
 
     rb_node* delete_min(rb_node* node){
         if(node->left == nullptr){
+            delete node;
             return nullptr;
         }
 
@@ -174,7 +211,7 @@ public:
             root->color =rb_node::red;
         }
         root = delete_min(root);
-        if(size() != 0){
+        if(root != nullptr){
             root->color = rb_node::black;
         }
     }
@@ -190,6 +227,7 @@ public:
     rb_node* delete_max(rb_node* node){
        
         if(is_red(node->left)){
+            delete node;
             node = rotate_right(node);
         }
 
@@ -210,7 +248,7 @@ public:
             root->color =rb_node::red;
         }
         root = delete_max(root);
-        if(size() != 0){
+        if(root != nullptr){
             root->color = rb_node::black;
         }
     }
@@ -234,14 +272,17 @@ public:
             }
 
             if(key == node->k){
-                node->v = get(node->right,min(node->right)->k);
-                node->key = min(node->right)->k;
+                
+                auto tmp = get(node->right,min(node->right)->k);
+                node->v = tmp.first;
+                node->k = min(node->right)->k;
                 node->right = delete_min(node->right);
             }else {
                 node->right = delete_node(node->right,key);
             }
-            return balance(node);
+           
         }
+        return balance(node);
     }
 
     void delete_node(K key){
@@ -249,11 +290,25 @@ public:
             root->color =rb_node::red;
         }
         root = delete_node(root,key);
-        if(size() == 0) {
+        if(root != nullptr) {
             root->color = rb_node::black;
         }
     }
 
+    void in_order(rb_node* node,std::vector<rb_node*>&  v){
+        if(node == nullptr){ return ;}
+        in_order(node->left,v);
+        v.push_back(node);
+        in_order(node->right,v);
+    }
+
+    std::vector<rb_node*> sort(){
+        std::vector<rb_node*> rlt;
+        in_order(root,rlt);
+        return rlt;
+    }
+
+   
 private:
     rb_node* root;
 
--- practice/test/test_red_black_tree.cpp
+++ practice/test/test_red_black_tree.cpp
@@ -1,6 +1,7 @@
 #include <catch.hpp>
 #include <red_black_tree.h>
 
+#if 1
 using namespace ez;
 TEST_CASE("test rb_node","[class]"){
     
@@ -24,11 +25,60 @@ TEST_CASE("test red_black_tree","[class]"){
         CHECK(test.size() == 0);
     }
 
+    SECTION("put"){
+        red_black_tree<int,char> test;
+        test.put(0,'0');
+        test.put(1,'1');
+        test.put(2,'2');
+        test.put(3,'3');
+        test.put(4,'4');
+        test.put(5,'5');
+        test.put(6,'6');
+        test.put(7,'7');
+        test.put(8,'8');
+        CHECK(test.size() == 9);
+        auto v = test.sort();
+        for(int i = 0; i< 9; ++i){
+            CHECK(v[i]->v == '0'+i);
+        }
+    }
 
+    
 
+    SECTION("delete"){
+        red_black_tree<int,char> test;
+        test.put(0,'0');
+        test.put(1,'1');
+        test.put(2,'2');
+        test.put(3,'3');
+        test.put(4,'4');
+        test.put(5,'5');
+        test.put(6,'6');
+        test.put(7,'7');
+        test.put(8,'8');
+        CHECK(test.size() == 9);
 
+        test.delete_node(8);
+        CHECK(test.size() == 8);
+        auto v = test.sort();
+        for(int i = 0; i< 8; ++i){
+            CHECK(v[i]->v == '0'+i);
+        }
+        test.delete_node(0);
+        test.delete_node(1);
+        test.delete_node(2);
+        test.delete_node(3);
+        test.delete_node(4);
+        test.delete_node(5);
+        test.delete_node(6);
+        test.delete_node(7);
+
+        CHECK(test.size() == 0);
+    }
 
 
 
 
 }
+
+#endif
